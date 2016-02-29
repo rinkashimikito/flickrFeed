b1bf58f16b3edafc6ba4efc2be3cd119b8b871a0
rinkashimikito <marcin@immedio.co.uk>
2016-02-29 21:03:35 +0000

    cleanup

--- www/js/app.js
+++ www/js/app.js
@@ -2,13 +2,12 @@
 
 angular.module('flickr', ['ionic'])
     .controller('ItemsCtrl', ['$scope', '$http', '$ionicModal', '$ionicSideMenuDelegate', function($scope, $http, $ionicModal, $ionicSideMenuDelegate) {
-        $scope.items =  [];
-        $scope.tag = '';
-        $scope.feed = {};
-
-        //if ($scope.updated)
-
-        $scope.flickrUrl = 'http://api.flickr.com/services/feeds/photos_public.gne?format=json&jsoncallback=JSON_CALLBACK&tags=';
+        $scope.flickr = {
+            items: [],
+            tag: '',
+            feed: {},
+            flickrUrl: 'http://api.flickr.com/services/feeds/photos_public.gne?format=json&jsoncallback=JSON_CALLBACK&tags='
+        };
 
         $ionicModal.fromTemplateUrl('new-tag.html', function(modal) {
             $scope.taskModal = modal;
@@ -21,14 +20,11 @@ angular.module('flickr', ['ionic'])
             $scope.code = null;
             $scope.response = null;
 
-            $http({method: 'jsonp', url: $scope.flickrUrl + $scope.tag}).
+            $http({method: 'jsonp', url: $scope.flickr.flickrUrl + $scope.flickr.tag}).
                 then(function(response) {
-                    $scope.status = response.status;
-                    $scope.data = response.data;
                     angular.forEach(response.data.items, function(item){
                         item.media.m = item.media.m.replace('_m.jpg', '.jpg');
-                        //item.htmlDesc = $sce.trustAsHtml(item.description);
-                        $scope.items.push(item);
+                        $scope.flickr.items.push(item);
                     });
                 }, function(response) {
                     $scope.data = response.data || "Request failed";
@@ -47,14 +43,14 @@ angular.module('flickr', ['ionic'])
         };
 
         $scope.createTag = function (tag) {
-            $scope.tag = tag;
-            $scope.items =  [];
+            $scope.flickr.tag = tag;
+            $scope.flickr.items =  [];
             $scope.fetch();
             $scope.taskModal.hide();
         };
 
         $scope.doRefresh = function() {
-            $scope.items =  [];
+            $scope.flickr.items =  [];
             $scope.fetch();
             $scope.$broadcast('scroll.refreshComplete');
             $scope.$apply()
